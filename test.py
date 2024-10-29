12b786c5a1e3fb1fdcd7b87bd6e7710bacb790e1
AJ Slater <aj@slater.net>
2024-10-28 22:33:47 -0700

    more modular

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 """Run validation on an xsd:assert example."""
+from pprint import pprint
+from xmlschema import XMLSchema11
 
-import xmlschema
+
+def test(schema, xml_fn):
+    """Validate and parse one xml file."""
+    schema.validate(xml_fn)
+    pprint(schema.to_dict(xml_fn))
 
 def main():
-    schema = xmlschema.XMLSchema11('ids.xsd')
-
-    print("This should pass:", flush=True)
-    schema.validate("ids_pass.xml")
-
-    print("This should fail:", flush=True)
-    schema.validate("ids_fail.xml")
+    """Run all tests."""
+    schema = XMLSchema11('ids.xsd')
+    print("This should pass:")
+    test(schema, "ids_pass.xml")
+    print()
+    print("This should fail:")
+    test(schema, "ids_fail.xml")
 
 
 if __name__ == "__main__":
